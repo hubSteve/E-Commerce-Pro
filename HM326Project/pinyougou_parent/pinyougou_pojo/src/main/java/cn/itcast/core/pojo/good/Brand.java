710c4f3b38d08343d344c8c55d340798e6ddca9e
zhang <1191094855@qq.com>
2018-11-26 18:00:58 +0800

    修改pojo

--- HM326Project/pinyougou_parent/pinyougou_pojo/src/main/java/cn/itcast/core/pojo/good/Brand.java
+++ HM326Project/pinyougou_parent/pinyougou_pojo/src/main/java/cn/itcast/core/pojo/good/Brand.java
@@ -15,6 +15,11 @@ public class Brand implements Serializable {
      */
     private String firstChar;
 
+    /**
+     * 状态
+     */
+    private String status;
+
     private static final long serialVersionUID = 1L;
 
     public Long getId() {
@@ -41,6 +46,14 @@ public class Brand implements Serializable {
         this.firstChar = firstChar == null ? null : firstChar.trim();
     }
 
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status == null ? null : status.trim();
+    }
+
     @Override
     public String toString() {
         StringBuilder sb = new StringBuilder();
@@ -50,6 +63,7 @@ public class Brand implements Serializable {
         sb.append(", id=").append(id);
         sb.append(", name=").append(name);
         sb.append(", firstChar=").append(firstChar);
+        sb.append(", status=").append(status);
         sb.append(", serialVersionUID=").append(serialVersionUID);
         sb.append("]");
         return sb.toString();
@@ -68,8 +82,9 @@ public class Brand implements Serializable {
         }
         Brand other = (Brand) that;
         return (this.getId() == null ? other.getId() == null : this.getId().equals(other.getId()))
-            && (this.getName() == null ? other.getName() == null : this.getName().equals(other.getName()))
-            && (this.getFirstChar() == null ? other.getFirstChar() == null : this.getFirstChar().equals(other.getFirstChar()));
+                && (this.getName() == null ? other.getName() == null : this.getName().equals(other.getName()))
+                && (this.getFirstChar() == null ? other.getFirstChar() == null : this.getFirstChar().equals(other.getFirstChar()))
+                && (this.getStatus() == null ? other.getStatus() == null : this.getStatus().equals(other.getStatus()));
     }
 
     @Override
@@ -79,6 +94,7 @@ public class Brand implements Serializable {
         result = prime * result + ((getId() == null) ? 0 : getId().hashCode());
         result = prime * result + ((getName() == null) ? 0 : getName().hashCode());
         result = prime * result + ((getFirstChar() == null) ? 0 : getFirstChar().hashCode());
+        result = prime * result + ((getStatus() == null) ? 0 : getStatus().hashCode());
         return result;
     }
 }
