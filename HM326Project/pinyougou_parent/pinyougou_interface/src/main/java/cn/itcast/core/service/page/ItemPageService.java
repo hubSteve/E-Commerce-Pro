257f4a00432d16763c3a0761a00347c78ac81058
123 <123@qq.com>
2018-11-26 13:37:06 +0800

    删除

--- HM326Project/pinyougou_parent/pinyougou_interface/src/main/java/cn/itcast/core/service/page/ItemPageService.java
+++
@@ -1,6 +0,0 @@
-package cn.itcast.core.service.page;
-
-public interface ItemPageService {
-
-    boolean getItemHtml(long goodsId);
-}
--- HM326Project/pinyougou_parent/pinyougou_interface/src/main/java/cn/itcast/core/service/search/ItemSearchService.java
+++
@@ -1,8 +0,0 @@
-package cn.itcast.core.service.search;
-
-import java.util.Map;
-
-public interface ItemSearchService {
-
-    Map<String,Object> search(Map<String,String> searchMap);
-}
